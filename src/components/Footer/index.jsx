6e6c18bc38c54bfbe2ee47f044066619a1f3f2c0
David R <egoz3ntrum@gmail.com>
2021-12-21 18:35:24 +0000

    removed some icons and added 42 logo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className={Styles.footer}>
       <p> &copy; {new Date().getFullYear()} · 42 Cybersec</p>
-      <img className={Styles.logo42} src="/public/assets/42_Logo.svg.png"></img>
+      <img className={Styles.logo42} src="public/assets/42_Logo.svg.png"></img>
       <small className={Styles.byline}>🚀 Powered by <a href="https://astro.build">Astro</a></small>
     </footer>
   );
