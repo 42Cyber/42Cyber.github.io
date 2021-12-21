07249ad5b0c49e70041b332c96091890ea28b063
David R <egoz3ntrum@gmail.com>
2021-12-21 15:35:25 +0100

    io

---
+++ src/components/PortfolioPreview/index.jsx
@@ -0,0 +1,28 @@
+import { h } from 'preact';
+import Styles from './styles.module.scss';
+
+function PortfolioPreview({ event }) {
+  return (
+    <div className={Styles.card}>
+      <div className={Styles.titleCard} style={`background-image:url(${event.img})`}>
+        <h1 className={Styles.title}>{event.title}</h1>
+      </div>
+      <div className="pa3">
+        <p className={`${Styles.desc} mt0 mb2`}>{event.description}</p>
+        <div className={Styles.tags}>
+          Tagged:
+          {event.tags.map((t) => (
+            <div className={Styles.tag} data-tag={t}>
+              {t}
+            </div>
+          ))}
+        </div>
+        <a className={Styles.link} href={event.url}>
+          <span className={Styles.linkInner}>View</span>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default PortfolioPreview;
---
+++ src/pages/event/microctf.md
@@ -0,0 +1,17 @@
+---
+layout: ../../layouts/event.astro
+title: MicroCTF
+client: Self
+publishDate: 2021-11-02 00:00:00
+img: https://images.unsplash.com/photo-1563089145-599997674d42?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80&auto=format&fit=crop&w=1974&q=80?fit=crop&w=1400&h=700&q=75
+description: |
+  El primer Capture The Flag celebrado en 42 Madrid
+tags:
+  - ctf
+  - 42
+  - eventos
+---
+
+Un CTF de iniciación basado en Linux en el que para avanzar hay que superar diferentes retos de programación, reversing y administración de sistemas UNIX.
+
+Próximamente habrá más ediciones.
